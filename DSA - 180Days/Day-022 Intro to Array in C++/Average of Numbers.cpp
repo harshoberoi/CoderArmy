6cbf6d4edbfe133bfa07529282719835227579ef
harshoberoi <121999815+harshoberoi@users.noreply.github.com>
2023-12-31 10:08:28 +0530

    Create Average of Numbers.cpp
    HomeWork

---
+++ DSA - 180Days/Day-022 Intro to Array in C++/Average of Numbers.cpp
@@ -0,0 +1,24 @@
+//Calculate the average of elements in an array of size 18.
+
+//AVERAGE 
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[18];
+      int ans=0,size=0; //to store sum value
+    
+    for(int i=0;i<18;i++)
+    {
+    cin>>arr[i];    
+    ans=ans+arr[i];
+    }
+size=sizeof(arr)/sizeof(arr[0]);
+
+    cout<<ans/size;
+
+}
+
+
+  
