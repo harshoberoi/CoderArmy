62c07232df48f9a01d3d4833a9b08134da55d142
harshoberoi <121999815+harshoberoi@users.noreply.github.com>
2024-01-08 11:56:58 +0530

    Question-6

---
+++ DSA - 180Days/Day-022 Intro to Array in C++/3rd Smallest Element.cpp
@@ -0,0 +1,40 @@
+
+//2nd smallest element
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{    
+    int n, first= INT_MAX;
+    cout<<"enter the size of array ";
+    cin>>n;
+
+    int arr[100];
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+
+    //first smallest element
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]<first)
+        first=arr[i];
+    }
+    //find 2nd smallest element
+    int second=INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        if(first!=arr[i])
+        second=min(second,arr[i]);
+    }
+
+
+    int third=INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        if(first!=arr[i] && second!=arr[i])
+        third=min(third,arr[i]);
+    }
+    cout<<third;
+    
+    
+    return 0;
+}
